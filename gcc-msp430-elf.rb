787f277470b175984c3112b968bbca0d820c1710
Gabriel Bauer <gabeb1277@gmail.com>
2026-06-13 22:58:01 -0400

    Undo latest changes

--- gcc-msp430-elf.rb
+++ gcc-msp430-elf.rb
@@ -1,10 +1,10 @@
 class GccMsp430Elf < Formula
-  desc "MSP430 GCC cross-compiler"
-  homepage "https://www.ti.com/tool/MSP430-GCC-OPENSOURCE"
-  
-  # Use the latest TI MSP430 GCC release (13.x series)
-  url "https://dr-download.ti.com/software-development/ide-configuration-compiler-or-debugger/MD-QnB99YsJRt/13.2.0.0/msp430-gcc-13.2.0.0_source.tar.bz2"
-  version "13.2.0.0"
+  homepage "http://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSPGCC/latest/index_FDS.html"
+  url "https://ftp.gnu.org/gnu/gcc/gcc-9.3.0/gcc-9.3.0.tar.xz"
+  mirror "https://ftpmirror.gnu.org/gcc/gcc-9.3.0/gcc-9.3.0.tar.xz"
+  sha256 "71e197867611f6054aa1119b13a0c0abac12834765fe2d81f35ac57f84f742d1"
+  version "9.3.1-11"
+  revision 1
 
   depends_on "binutils-msp430-elf"
   depends_on "headers-msp430-elf"
@@ -13,34 +13,95 @@ class GccMsp430Elf < Formula
   depends_on "libmpc" => :build if OS.mac?
   depends_on "isl" => :build if OS.mac?
   depends_on "gcc" => :build if OS.mac?
-  
+
+  patch :p0 do
+    url "https://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSPGCC/9_3_1_2/export/msp430-gcc-9.3.1.11-source-patches.tar.bz2"
+    sha256 "ec6472b034e11e8cfdeb3934b218e5bafbb7a03f3afc0e76536bd9c42653525b"
+    apply "gcc-9.3.0.patch"
+  end
+
+  resource "newlib" do
+    url "ftp://sourceware.org/pub/newlib/newlib-2.4.0.tar.gz"
+    sha256 "545b3d235e350d2c61491df8b9f775b1b972f191380db8f52ec0b1c829c52706"
+
+    patch :p0 do
+      url "https://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSPGCC/9_3_1_2/export/msp430-gcc-9.3.1.11-source-patches.tar.bz2"
+      sha256 "ec6472b034e11e8cfdeb3934b218e5bafbb7a03f3afc0e76536bd9c42653525b"
+      apply "newlib-2_4_0.patch"
+    end
+  end
+
   def install
     target = "msp430-elf"
-    
-    # Configure with modern options
+
+    resource("newlib").stage do
+      buildpath.install "newlib"
+      buildpath.install "libgloss"
+    end
+
+    gmp = Formula["gmp"] if OS.mac?
+    mpfr = Formula["mpfr"] if OS.mac?
+    libmpc = Formula["libmpc"] if OS.mac?
+    isl = Formula["isl"] if OS.mac?
+
     on_macos do
-      system "./configure",
-        "--target=#{target}",
-        "--prefix=#{prefix}",
-        "--enable-languages=c,c++",
-        "--disable-bootstrap",
-        "--with-gmp=#{Formula["gmp"].opt_prefix}",
-        "--with-mpfr=#{Formula["mpfr"].opt_prefix}",
-        "--with-mpc=#{Formula["libmpc"].opt_prefix}"
-    end
-    on_linux do
-      system "./configure",
-        "--target=#{target}",
-        "--prefix=#{prefix}",
-        "--enable-languages=c,c++",
-        "--disable-bootstrap"
-    end
+      # Use GCC instead of Clang - this avoids the islower macro conflict
+      # Install GCC if not already present - macOS
+      gcc = Formula["gcc"]
+      ENV["CC"] = gcc.opt_bin/"gcc-#{gcc.version.major}"
+      ENV["CXX"] = gcc.opt_bin/"g++-#{gcc.version.major}"
     
-    system "make", "-j#{ENV.make_jobs}"
-    system "make", "install"
-  end
-  
-  test do
-    system "#{bin}/msp430-elf-gcc", "--version"
+      # Critical: Undefine the problematic macros before building
+      ENV["CFLAGS"] = "-U_FORTIFY_SOURCE -D_POSIX_C_SOURCE=200809L"
+      ENV["CXXFLAGS"] = "-U_FORTIFY_SOURCE -D_POSIX_C_SOURCE=200809L"
+    end
+
+    # gcc must be built outside of the source directory.
+    mkdir "build" do
+      on_macos do
+        system "../configure",
+          "--target=#{target}",
+          "--program-prefix=#{target}-",
+          "--prefix=#{prefix}",
+          "--enable-languages=c,c++",
+          "--disable-nls",
+          "--enable-inifini-array",
+          "--enable-target-optspace",
+          "--enable-newlib-nano-formatted-io",
+          "--with-system-zlib",
+          "--with-as=#{HOMEBREW_PREFIX}/bin/#{target}-as",
+          "--with-ld=#{HOMEBREW_PREFIX}/bin/#{target}-ld",
+          "--with-gmp=#{gmp.prefix}",
+          "--with-mpfr=#{mpfr.prefix}",
+          "--with-mpc=#{libmpc.prefix}",
+          "--disable-libstdcxx-pch",
+          "--disable-build-poststage1-with-cxx",
+          "--disable-host-shared"
+      end
+      on_linux do
+        system "../configure",
+          "--target=#{target}",
+          "--program-prefix=#{target}-",
+          "--prefix=#{prefix}",
+          "--enable-languages=c,c++",
+          "--disable-nls",
+          "--enable-inifini-array",
+          "--enable-target-optspace",
+          "--enable-newlib-nano-formatted-io",
+          "--with-system-zlib",
+          "--with-as=#{HOMEBREW_PREFIX}/bin/#{target}-as",
+          "--with-ld=#{HOMEBREW_PREFIX}/bin/#{target}-ld"
+      end
+      system "make"
+      system "make", "install"
+    end
+
+    # Remove unnecessary files.
+    info.rmtree
+    man7.rmtree
+
+    # Create symlinks to linker scripts from headers-msp430-elf.
+    ldscripts = "#{HOMEBREW_PREFIX}/lib/#{target}/lib/ldscripts"
+    (prefix/target/"lib").install_symlink Dir["#{ldscripts}/*.ld"]
   end
 end
